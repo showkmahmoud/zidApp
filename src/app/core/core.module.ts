074872bbf72dedcbef6aa3afa39c409e57c6bc41
showk mahmoud <showkmahmoud@gmail.com>
2022-07-01 21:11:38 +0200

    add assets

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     CoreRoutingModule
