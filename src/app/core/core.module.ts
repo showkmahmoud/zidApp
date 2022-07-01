eb9af38a3f430bcc64bae38771cd831a0a2d20a5
showk mahmoud <showkmahmoud@gmail.com>
2022-07-01 22:04:46 +0200

    add the global style & the routes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,9 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
-
 @NgModule({
-  declarations: [
-    NavbarComponent
-  ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [NavbarComponent],
+  imports: [CommonModule, CoreRoutingModule],
+  exports: [NavbarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PagesRoutingModule
-  ]
+  declarations: [MainPageComponent],
+  imports: [CommonModule, PagesRoutingModule, CoreModule],
 })
-export class PagesModule { }
+export class PagesModule {}
